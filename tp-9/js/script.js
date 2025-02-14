669ef004a70c0713ed8af2851115a41bec4ab242
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-02-14 22:43:32 +0100

    array concat exo-9

---
+++ tp-9/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Crée deux tableaux groupeA et groupeB contenant des prénoms.
+2. Fusionne-les dans un nouveau tableau tousLesEleves.
+3. Affiche le tableau final dans la console
+ */
+
+const groupeA = [ "pc", "raspi", "keys", "mouse"];
+const groupeB = ["apache", "kali", "parrot", "ubuntu", "12"];
+const valeurA = [...groupeA.values()];// Convertir l'itérateur en tableau
+const valeurB = [...groupeB.values()];
+console.log(valeurA.concat(valeurB));
